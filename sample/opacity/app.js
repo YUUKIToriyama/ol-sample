edf587dabed9f6d8497b45f218744c79343f53ff
kohei-kp <cowhey1991@gmail.com>
2016-08-24 16:02:06 +0900

    MapQuest削除

--- sample/opacity/app.js
+++ sample/opacity/app.js
@@ -1,9 +1,5 @@
 var osmSource = new ol.source.OSM();
-var mapQSource = new ol.source.MapQuest({ layer: 'sat' });
-
 var osmLayer = new ol.layer.Tile({ source: osmSource });
-var mapQLayer = new ol.layer.Tile({ source: mapQSource });
-
 var map = new ol.Map({ target: 'map'});
 
 map.setView(new ol.View({
@@ -11,9 +7,7 @@ map.setView(new ol.View({
   zoom: 2
 }));
 
-map.addLayer(mapQLayer);
 map.addLayer(osmLayer);
-
 
 var osmSlide   = document.getElementById('osm-slide');
 var osmOpacity = document.getElementById('osm-opacity');
@@ -22,27 +16,11 @@ osmSlide.addEventListener('input', function (e) {
   osmLayer.setOpacity(this.value);
 });
 
-var questSlide   = document.getElementById('mapquest-slide');
-var questOpacity = document.getElementById('mapquest-opacity');
-questSlide.addEventListener('input', function (e) {
-  questOpacity.innerHTML = this.value;
-  mapQLayer.setOpacity(this.value);
-});
-
 var osmOnOff   = document.getElementById('osm-vis');
 osmOnOff.addEventListener('click', function (e) {
   if (osmLayer.getVisible()) {
     osmLayer.setVisible(false);
   } else {
     osmLayer.setVisible(true);
-  }
-});
-
-var questOnOff = document.getElementById('mapquest-vis');
-questOnOff.addEventListener('click', function (e) {
-  if (mapQLayer.getVisible()) {
-    mapQLayer.setVisible(false);
-  } else {
-    mapQLayer.setVisible(true);
   }
 });
